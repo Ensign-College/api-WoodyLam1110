d1c7dad6d77731ecda2b71a345ed94ebed1b1d7a
WoodyLam1110 <121983241+WoodyLam1110@users.noreply.github.com>
2024-01-22 11:07:11 -0700

    update database

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-const express = require('express');
-//express make apis - connect frontend to database
+const express = require('express');//express make apis - connect frontend to database
+const Redis =require('redis');//import the Redis library
+
+
+const redisClient =Redis.createClient({
+    url:`redis://localhost:6379`
+
+});
 
 const app = express();//create an express application
+const port =3000; //port number
+app.listen(port,()=>{
+    redisClient.connect();
+    console.log (`API is Listening on port:${port}`);
+}); // listen for web requests from the frontend and don't stop
 
-app.listen(3000); // listen for web requests from the frontend and don't stop
+// const boxes = [
 
-const boxes = [
-    {boxId:1},
-    {boxId:2},
-    {boxId:3},
-    {boxId:4},
-];
+//     // {boxId:1},
+//     // {boxId:2},
+//     // {boxId:3},
+//     // {boxId:4},
+// ];hard codeed boxes - not in the database
 //1-url
 //2- a function to return boxes
 //req = the request from the browser
 //res = the response to the browser
-app.get('/boxes',(req,res)=>{//return boxes to the user
+app.get('/boxes',(req,res)=>{//return boxes to the user\
+    let boxes = await.redisClient.json.get('boxes',{path:'$'});//get the boxes
+    //send boxes to the browser
     res.send(JSON.stringify(boxes));//convert boes to a string
-});
+}); 
 console.log("Hello");
